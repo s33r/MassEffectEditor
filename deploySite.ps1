2b96893c178c66951d74e72aaca813c1a2d12632
Aaron Willows <aaron@aaronwillows.com>
2021-07-25 19:56:49 -0700

    Adding git fetch to deploy script

--- deploySite.ps1
+++ deploySite.ps1
@@ -4,6 +4,8 @@ git config --global user.name 'Aaron Willows'
 git config --global user.email 'aaron@aaronwillows.com'
 
 cp -r ./Aaron.MassEffectEditor.WebUI/bin/Debug/net5.0/publish/wwwroot ./docs
+
+git fetch
 git switch site
 git add docs
 git commit -m "Release $messageDate"

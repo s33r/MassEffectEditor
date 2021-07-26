22ed3ee5b96f756f18ae8e3dfc081853831b6be7
Aaron Willows <aaron@aaronwillows.com>
2021-07-25 20:23:16 -0700

    fixed command order in deploy script

--- deploySite.ps1
+++ deploySite.ps1
@@ -3,11 +3,13 @@ $messageDate=Get-Date -Format "yyyy-MM-dd HHmm"
 git config --global user.name 'Aaron Willows'
 git config --global user.email 'aaron@aaronwillows.com'
 
+git fetch
+git switch site
+
 Remove-Item ./docs -Recurse -Force
 cp -r ./Aaron.MassEffectEditor.WebUI/bin/Debug/net5.0/publish/wwwroot ./docs
 
-git fetch
-git switch site
+
 git add docs
 git commit -m "Release $messageDate"
 git push

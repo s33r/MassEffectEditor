51f5da52b74006e24fb42c424014f97c0c61ef36
Aaron Willows <aaron@aaronwillows.com>
2021-07-25 19:16:44 -0700

    Updated deploy script to run every step

--- deploySite.ps1
+++ deploySite.ps1
@@ -1,6 +1,7 @@
+$messageDate = Get-Date -Format "yyyy-MM-dd HHmm"
+
 cp -r ./Aaron.MassEffectEditor.WebUI/bin/Debug/net5.0/publish/wwwroot ./docs
 git switch site
-
-# git stage ./docs/*
-# git commit
-# git push
+git add docs
+git commit -m "Release $messageDate"
+git push

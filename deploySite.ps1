761a85527a8b8b0236e01ee24ba560270c7b8e8b
Aaron Willows <aaron@aaronwillows.com>
2021-07-25 20:14:51 -0700

    delete site files during deploy

--- deploySite.ps1
+++ deploySite.ps1
@@ -3,6 +3,7 @@ $messageDate=Get-Date -Format "yyyy-MM-dd HHmm"
 git config --global user.name 'Aaron Willows'
 git config --global user.email 'aaron@aaronwillows.com'
 
+rm -rf ./docs
 cp -r ./Aaron.MassEffectEditor.WebUI/bin/Debug/net5.0/publish/wwwroot ./docs
 
 git fetch
